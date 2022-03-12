17c95df42c4bc85b6b4d1a8ae187ad468e4dcd92
williamsilveiraf <williamsilveiraf@hotmail.com>
2022-03-12 13:35:04 -0300

    Backup

--- Datapath.vhd
+++ Datapath.vhd
@@ -69,6 +69,45 @@ component mux4x1bits16 is port (
     dataout: out std_logic_vector(15 downto 0));
 end component;
 
+component Comp_0 is port (        
+    USER: in std_logic_vector(3 downto 0);
+    CODE: in std_logic_vector(3 downto 0);
+    ISEQUAL: out std_logic);
+end component;
+
+component Comp_1 is port (        
+    USER: in std_logic_vector(7 downto 4);
+    CODE: in std_logic_vector(7 downto 4);
+    ISEQUAL: out std_logic);
+end component;
+
+component Comp_2 is port (        
+    USER: in std_logic_vector(11 downto 8);
+    CODE: in std_logic_vector(11 downto 8);
+    ISEQUAL: out std_logic);
+end component;
+
+component Comp_3 is port (        
+    USER: in std_logic_vector(15 downto 12);
+    CODE: in std_logic_vector(15 downto 12);
+    ISEQUAL: out std_logic);
+end component;
+
+component Soma_P is port (
+    P0, P1, P2, P3: in std_logic;
+    P: out std_logic_vector(2 downto 0));
+end component;
+
+component comp_e is port (    
+    inc, inu: in  std_logic_vector(15 downto 0);
+    E : out std_logic_vector(2 downto 0));
+end component;
+
+component bcd7seg is port (
+    bcd_in:  in std_logic_vector(3 downto 0);
+    out_7seg:  out std_logic_vector(6 downto 0));
+end component;
+
 begin
 
 end_game <= end_gamee; --ao interligar a saida do comp=4, usar o signal end_gamee para evitar erros
@@ -94,28 +133,94 @@ end_time <= end_timee; --ao interligar a saida do counter_time, usar o signal en
     -- Get rom sequence by sel key
     ROM0Sequence: ROM0 port map(
                                 address => sel(5 downto 2), 
-                                data => rom0_s);
+                                data => rom0_s(15 downto 0));
 
     ROM1Sequence: ROM1 port map(
                                 address => sel(5 downto 2), 
-                                data => rom1_s);
+                                data => rom1_s(15 downto 0));
 
     ROM2Sequence: ROM2 port map(
                                 address => sel(5 downto 2), 
-                                data => rom2_s);
+                                data => rom2_s(15 downto 0));
 
     ROM3Sequence: ROM3 port map(
                                 address => sel(5 downto 2), 
-                                data => rom3_s);
+                                data => rom3_s(15 downto 0));
 
     --Select CODE by ROM trough sel
     GetCodeSelected: mux4x1bits16 port map(
-        in00 => rom0_s, 
-        in01 => rom1_s, 
-        in10 => rom2_s, 
-        in11 => rom3_s, 
+        in00 => rom0_s(15 downto 0), 
+        in01 => rom1_s(15 downto 0), 
+        in10 => rom2_s(15 downto 0), 
+        in11 => rom3_s(15 downto 0), 
         SEL_MUX_ROM => sel(1 downto 0), 
-        dataout => code);
+        dataout => code(15 downto 0));
 
-    ledr <= code;
+    IsEqualSequence0: Comp_0 port map(        
+        USER => user(3 downto 0),
+        CODE => code(3 downto 0),
+        ISEQUAL => cmp0_s);
+    
+    IsEqualSequence1: Comp_1 port map(
+        USER => user(7 downto 4),
+        CODE => code(7 downto 4),
+        ISEQUAL => cmp1_s);
+
+    IsEqualSequence2: Comp_2 port map(
+        USER => user(11 downto 8),
+        CODE => code(11 downto 8),
+        ISEQUAL => cmp2_s);
+
+    IsEqualSequence3: Comp_3 port map(
+        USER => user(15 downto 12),
+        CODE => code(15 downto 12),
+        ISEQUAL => cmp3_s);
+
+    SumElementsEquals: Soma_P port map(
+        P0 => cmp0_s, 
+        P1 => cmp1_s, 
+        P2 => cmp2_s, 
+        P3 => cmp3_s, 
+        P => P(2 downto 0));
+
+    IsExistInMyCode: comp_e port map(
+        inc => code(15 downto 0), 
+        inu => user(15 downto 0), 
+        E => E(2 downto 0));
+
+
+    fHEX0: bcd7seg port map (    
+        bcd_in => code(3 downto 0),
+        out_7seg => hex0(6 downto 0));
+
+    fHEX1: bcd7seg port map (    
+        bcd_in => code(7 downto 4),
+        out_7seg => hex1(6 downto 0));
+
+    fHEX2: bcd7seg port map (    
+        bcd_in => code(11 downto 8),
+        out_7seg => hex2(6 downto 0));
+    
+    fHEX3: bcd7seg port map (    
+        bcd_in => code(15 downto 12),
+        out_7seg => hex3(6 downto 0));
+    
+    fHEX4: bcd7seg port map (    
+        bcd_in => user(3 downto 0),
+        out_7seg => hex4(6 downto 0));
+
+    fHEX5: bcd7seg port map (    
+        bcd_in => user(7 downto 4),
+        out_7seg => hex5(6 downto 0));
+
+    fHEX6: bcd7seg port map (    
+        bcd_in => user(11 downto 8),
+        out_7seg => hex6(6 downto 0));
+    
+    fHEX7: bcd7seg port map (    
+        bcd_in => user(15 downto 12),
+        out_7seg => hex7(6 downto 0));
+
+    ledr(2 downto 0) <= P(2 downto 0);
+    ledr(5 downto 3) <= E(2 downto 0);
 end arc_data;
